c11e562491708b3a534cbb5a3a4a7935f0e8ff1b
RabieImghi <rabieaitimghi7@gmail.com>
2024-03-05 17:08:52 +0100

    add gestion evenment for organisateur

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -50,6 +50,7 @@
                                     <ul class="submenu">
                                         <li><a href="/Events">All Event</a></li>
                                         <li><a href="/AddEvent">Add Event</a></li>
+                                        <li><a href="/DisplayMyEvent">My Events</a></li>
                                         <li><a href="/MyEventReservation/{{ Auth::user()->id }}">Accepte Reservation</a></li>
                                     </ul>
                                 </li>
